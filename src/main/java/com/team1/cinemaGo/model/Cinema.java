9ca8cf1f0ff521d755b1a365af130e43c976d9a2
nepiktaz <kveselis@gmail.com>
2014-12-05 21:20:00 +0100

    some JPA annotation added

--- src/main/java/com/team1/cinemaGo/model/Cinema.java
+++ src/main/java/com/team1/cinemaGo/model/Cinema.java
@@ -1,24 +1,34 @@
 package com.team1.cinemaGo.model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name="cinema")
+@Table(name="t_cinema")
 public class Cinema {
 	
+	@Id
+	@Column(name="cinema_id")
+	@GeneratedValue(strategy=GenerationType.AUTO)
 	private int id;
+	
+	@Column(name="cinema_name")	
 	private String cinemaName;
-	private int seatsNumber;
+
+	@Column(name="rows_count")	
+	private int rowsCount;
+	@Column(name="columns_count")	
+	private int columnsCount;
+
 	
 	public String getCinemaName() {
 		return cinemaName;
 	}
 	
-	@Id
-	@GeneratedValue
 	public int getId() {
 		return id;
 	}
@@ -28,17 +38,30 @@ public class Cinema {
 	public void setCinemaName(String cinemaName) {
 		this.cinemaName = cinemaName;
 	}
-	public int getSeatsNumber() {
-		return seatsNumber;
+
+	public int getRowsCount() {
+		return rowsCount;
 	}
-	public void setSeatsNumber(int seatsNumber) {
-		this.seatsNumber = seatsNumber;
+
+	public void setRowsCount(int rowsCount) {
+		this.rowsCount = rowsCount;
 	}
+
+	public int getColumnsCount() {
+		return columnsCount;
+	}
+
+	public void setColumnsCount(int columnsCount) {
+		this.columnsCount = columnsCount;
+	}
+
 	@Override
 	public String toString() {
-		return "Cinema [cinemaName=" + cinemaName + ", seatsNumber="
-				+ seatsNumber + "]";
+		return "Cinema [id=" + id + ", cinemaName=" + cinemaName
+				+ ", rowsCount=" + rowsCount + ", columnsCount=" + columnsCount
+				+ "]";
 	}
+
 
 	
 }
--- src/main/java/com/team1/cinemaGo/model/Movie.java
+++ src/main/java/com/team1/cinemaGo/model/Movie.java
@@ -2,14 +2,44 @@ package com.team1.cinemaGo.model;
 
 import java.time.Duration;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="t_movie")
 public class Movie {
 	
+	@Id
+	@Column(name="movie_id")
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private int id;
+	
+	@Column(name="movie_title")
 	private String movieTitle;
+	
+	@Column(name="movie_duration")
 	private Duration duration;
+	
+	@Column(name="movie_description")
 	private String description;
+	
+	
 	public String getMovieTitle() {
 		return movieTitle;
 	}
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
 	public void setMovieTitle(String movieTitle) {
 		this.movieTitle = movieTitle;
 	}
@@ -31,7 +61,4 @@ public class Movie {
 				+ ", description=" + description + "]";
 	}
 	
-	
-	
-
 }
