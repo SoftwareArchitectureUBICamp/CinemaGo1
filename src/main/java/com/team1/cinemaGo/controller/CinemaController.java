2e246340614b5a02b17453f36fe18a8c1532de7d
nepiktaz <kveselis@gmail.com>
2014-12-20 13:02:18 +0200

    Cinema, Movies controllers updated

--- src/main/java/com/team1/cinemaGo/controller/CinemaController.java
+++ src/main/java/com/team1/cinemaGo/controller/CinemaController.java
@@ -1,15 +1,19 @@
 package com.team1.cinemaGo.controller;
 
-import java.util.Map;
+import java.util.List;
 
-import javax.servlet.http.HttpServletRequest;
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.team1.cinemaGo.model.Cinema;
+import com.team1.cinemaGo.service.CinemaService;
 
 
 @Controller
@@ -17,49 +21,46 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class CinemaController {
 
 	
-	@RequestMapping("/hello/{name}")
-	public String hello(@PathVariable("name") String name, Model model) {
-		model.addAttribute("name", name);
-		return "hello";
+	@Autowired(required=true)
+	@Qualifier(value="cinemaService")
+	private CinemaService cinemaService;
+    
+    public void setCinemaService(CinemaService cs){
+        this.cinemaService = cs;
+    }
+
+    @RequestMapping(value="/", method=RequestMethod.GET)
+    public String getCinemas(Model model) {
+    	
+    	List<Cinema> cinemas = cinemaService.listCinemas();
+		model.addAttribute("cinema",new Cinema());
+    	model.addAttribute("cinemas", cinemas);
+    	
+		return "cinemas";
 	}
 
-	@RequestMapping("/{next}/{kitas}")
-	public String next(@PathVariable Map<String, String> pathVars , Model model) {
+    @RequestMapping(value="/get/{id}", method=RequestMethod.GET)
+    public String getCinema(@PathVariable int id, Model model) {
+		Cinema cinema = cinemaService.getCinemaById(id);
+		model.addAttribute("cinema", cinema);		
 		
-		String next = pathVars.get("next");
-		String kitas = pathVars.get("kitas");
+		return "cinemasForm";
+	}
+    
+    @RequestMapping(value="/delete/{id}", method=RequestMethod.GET)
+    public String removeCinema(@PathVariable int id, Model model) {
 		
-		model.addAttribute("msg", "Nextas yra: " + next);
-		model.addAttribute("kitas", "O va kitas yra: " + kitas);
-		
-		return "next";
+    	cinemaService.removeCinema(id);
+		return "redirect:/cinemas/";
 		
 	}
-	
-	
-	@RequestMapping(value="/form.html", method=RequestMethod.GET)
-	public String form(Model model) {
-		
-		
-		return "simpleForm";
-	
-	}
-	
-	@RequestMapping(value="/submitForm.html", method=RequestMethod.POST)
-	public String submit(HttpServletRequest request, Model model) {
-		
-		String name = (String) request.getParameter("name");
-		
-		model.addAttribute("name", name);
-		
-		return "formResult";
-	
+ 
+    @RequestMapping(value="/save", method=RequestMethod.POST)
+    public String setMovie(@ModelAttribute("cinema") Cinema cinema, BindingResult result) {
+
+    	cinemaService.addCinema(cinema);
+		return "redirect:/cinema/";
 	}
 
-
-	@ModelAttribute
-	public void addTitle(Model model){
-		model.addAttribute("head", "Hederis viskam");
-	}
 	
 }
--- src/main/java/com/team1/cinemaGo/controller/MovieController.java
+++ src/main/java/com/team1/cinemaGo/controller/MovieController.java
@@ -2,58 +2,64 @@ package com.team1.cinemaGo.controller;
 
 import java.util.List;
 
-import javax.transaction.Transactional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
-
-
-
-
-
-
-
-
-
-import com.team1.cinemaGo.dao.MovieDAO;
 import com.team1.cinemaGo.model.Movie;
-import com.team1.cinemaGo.service.CinemaService;
 import com.team1.cinemaGo.service.MovieService;
 
 
-//@RestController 
 @Controller 
 @RequestMapping(value="/movies")
 public class MovieController {
 	
 	@Autowired(required=true)
+	@Qualifier(value="movieService")
 	private MovieService movieService;
     
     public void setMovieService(MovieService ms){
         this.movieService = ms;
     }
-    
-    @RequestMapping(method=RequestMethod.GET)
-    public List<Movie> getMovies(Model model) {
-    	List<Movie> movies = movieService.listMovie();
-		model.addAttribute("movies", movies);
-    	return movies;
-	}
 
-    @RequestMapping(value="/{id}", method=RequestMethod.GET)
-    public String getMovie(@PathVariable int id, Model model) {
-		Movie movie = movieService.getMovieById(id);
-		model.addAttribute("movie", movie);		
+    @RequestMapping(value="/", method=RequestMethod.GET)
+    public String getMovies(Model model) {
+    	
+    	List<Movie> movies = movieService.listMovie();
+		model.addAttribute("movie",new Movie());
+    	model.addAttribute("movies", movies);
+    	
 		return "movies";
 	}
 
+    @RequestMapping(value="/get/{id}", method=RequestMethod.GET)
+    public String getMovie(@PathVariable int id, Model model) {
+		Movie movie = movieService.getMovieById(id);
+		model.addAttribute("movie", movie);		
+		
+		return "moviesForm";
+	}
+    
+    @RequestMapping(value="/delete/{id}", method=RequestMethod.GET)
+    public String removeMovie(@PathVariable int id, Model model) {
+		
+    	movieService.removeMovie(id);
+		return "redirect:/movies/";
+		
+	}
+ 
+    @RequestMapping(value="/save", method=RequestMethod.POST)
+    public String setMovie(@ModelAttribute("movie") Movie movie, BindingResult result) {
+
+    	movieService.addMovie(movie);
+    	
+		return "redirect:/movie/";
+	}
 
 }
